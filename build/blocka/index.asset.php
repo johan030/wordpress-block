4c0ed1117e1cb49357f3ac89317b107894001238
Johan Callens <weylin030@protonmail.ch>
2022-09-27 09:50:11 +0200

    adding success block and danger block

---
+++ build/blocka/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '21f1b7955e74dbe4868f');
---
+++ build/blockb/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0aee47a0c3430dd6544d');
